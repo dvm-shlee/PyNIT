2ee214ad82a724c01fa5e02be6cfa962418c0224
dvm-shlee <shlee@unc.edu>
2016-09-02 01:39:44 -0400

    GLM bug patch #8

--- pynit/core/project.py
+++ pynit/core/project.py
@@ -930,9 +930,9 @@ class Preprocess(object):
                 for i, finfo in funcs.iterrows():
                     print(" +Filename: {}".format(finfo.Filename))
                     output_path = os.path.join(step01, subj, finfo.Filename)
-                    self._prjobj.run('afni_3dDeconvolve', '{}'.format(output_path), finfo.Abspath, nfirst=0,
-                                     polort=-1, num_stimts=1, stim_file=[1, paradigm],
-                                     stim_label=[1, 'STIM'], num_glt=1, glt_label=[1, 'STIM'],
+                    self._prjobj.run('afni_3dDeconvolve', output_path, finfo.Abspath, nfirst='0',
+                                     polort='-1', num_stimts='1', stim_file=['1', str(paradigm)],
+                                     stim_label=['1', 'STIM'], num_glt='1', glt_label=['1', 'STIM'],
                                      gltsym='SYM: +STIM', tout=True)
         return {'func': step01}
 
