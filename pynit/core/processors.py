ae5c9fbc32f7f6303b3d5cefa1decb066920f7d8
dvm-shlee <shlee@unc.edu>
2017-01-26 20:40:37 -0500

    correlation

--- pynit/core/processors.py
+++ pynit/core/processors.py
@@ -98,8 +98,10 @@ class Analysis(object):
             df = Interface.afni_3dROIstats(None, input_file, mask_file)
             df.columns = list_of_rois
             # mask_file.close()
-            tempobj.atlas.flip(invertx=True)
+            # tempobj.atlas.flip(invertx=True)
             list_of_rois = ['contra_'+roi[0] for roi in tempobj.label.itervalues()][1:]
+            tempobj.atlas._dataobj = tempobj.atlas._dataobj[::-1, :, :]
+            print('Flipped')
             mask2_file = TempFile(tempobj.atlas, filename='mask2')
             cont_df = Interface.afni_3dROIstats(None, input_file, mask2_file)
             cont_df.columns = list_of_rois
