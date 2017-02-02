f61366f9c2444a2542cd391596ec753c0f793709
dvm-shlee <shlee@unc.edu>
2017-02-02 13:39:08 -0500

    3dTproject update (170131)

--- pynit/core/handlers.py
+++ pynit/core/handlers.py
@@ -1903,6 +1903,7 @@ class Preprocess(object):
                     if padded:
                         exec('ss_epi.crop({}=[1, {}])'.format(axis[zaxis], ss_epi.shape[zaxis]-1))
                     ss_epi.save_as(os.path.join(step01, subj, filename), quiet=True)
+                    del ss_epi
                     os.remove(fpath)
                 for i, finfo in t2:
                     print(" +Filename of anat: {}".format(finfo.Filename))
@@ -1914,6 +1915,7 @@ class Preprocess(object):
                     if padded:
                         exec('ss_t2.crop({}=[1, {}])'.format(axis[zaxis], ss_t2.shape[zaxis] - 1))
                     ss_t2.save_as(os.path.join(step02, subj, filename), quiet=True)
+                    del ss_t2
                     os.remove(fpath)
             else:
                 for sess in self.sessions:
