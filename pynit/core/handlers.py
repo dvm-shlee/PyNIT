f4636581f9f12ec5a0bf7a9ca332e6773b38caff
dvm-shlee <jaru419@gmail.com>
2017-03-04 18:01:29 -0500

    Pipeline update

--- pynit/core/handlers.py
+++ pynit/core/handlers.py
@@ -1676,7 +1676,7 @@ class Pipelines(object):
         :param kwargs:
         :return:
         """
-        self._prjobj.reset(True)
+        self._prjobj.reload()
         if isinstance(pipeline, int):
             pipeline = self.avail[pipeline]
         if pipeline in self.avail.values():
