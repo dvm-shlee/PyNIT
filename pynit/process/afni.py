6d6cc19dcba3219d338c24a4cca15a03540f5963
SungHo Lee <jaru419@gmail.com>
2018-03-03 15:06:31 -0500

    180303(0.1.16)#1 - afni.py::afni_TemporalClipping : allow multiple range

--- pynit/process/afni.py
+++ pynit/process/afni.py
@@ -925,8 +925,7 @@ class AFNI_Process(BaseProcess):
         func = self.check_input(func)
         step.set_input(name='func', path=func, filters=kwargs)
         step.set_output(name='output', ext='remove')
-        print(self.ext)
-        step.set_var(name='ext', value=self.ext)
+        step.set_var(name='ext', value=self.ext[-1])
         if mparam:
             step.set_input('mparam', path=func, filters=dict(ext='.1D', ignore='aff12'), type=1)
         if clip_range:
